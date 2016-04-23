ab1d24047414b7e4410c8f2bf140b651561cbe72
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-04-23 14:01:59 +1000

    started building updated search page

--- pages/search.php
+++ pages/search.php
@@ -1,27 +1,25 @@
 <?php include "../controllers/search.ctrl.php" ?>
 <head>
-	<style type="text/css"> body {font-family:Arial, Sans-Serif;} #container {width:300px; margin:250 auto;} form label {display:inline-block; width:140px;}
-   form input[type="text"], form input[type="password"], form input[type="email"] {width:160px;} form .line {clear:both;}
-   form .line.submit {text-align:right;} </style>
+	<link rel="stylesheet" href="../css/style.css" media="screen" title="no title" charset="utf-8">
 </head>
 
 <body>
 <div id ="container">
 	<form>
 		Name: <select id="namelist" name = "namelist" form="nameform">
-				<option value ="-">-</option>
-				<option value ="bob">Bob</option>
-				<option value ="mark">Mark</option>
+				<option value ="" disabled="" selected="" hidden="">Name</option>
+				<option value ="Bob">Bob</option>
+				<option value ="Mark">Mark</option>
 				<option value ="123456789">123456789101112</option>
 			</select><br>
 		Surname: <select id="surnamelist" name = "surnamelist" form="surnameform">
-				<option value ="-          ">-</option>
+				<option value ="" disabled="" selected="" hidden="">Surname</option>
 				<option value ="Johnson">Johnson</option>
 				<option value ="Smith">Smith</option>
 				<option value ="123456789">123456789101112</option>
 				</select><br>
 		Role: <select id="rolelist" name = "rolelist" form="roleform">
-				<option value ="-">-</option>
+				<option value ="" disabled="" selected="" hidden="">Role</option>
 				<option value ="sales manager">Sales Manager</option>
 				<option value ="boss">Boss</option>
 				<option value ="123456789">123456789101112</option>
@@ -29,7 +27,4 @@
 
 	</form>
 </div>
-
-
-
 </body>
---
+++ pages/searchBar.php
@@ -0,0 +1,21 @@
+<?php include "../controllers/search.ctrl.php" ?>
+
+<head>
+	<link rel="stylesheet" href="../css/style.css" media="screen" title="no title" charset="utf-8">
+	<head>
+	  <meta charset="utf-8">
+	  <title>Orientanda - </title>
+	  <link rel="stylesheet" href="../css/style.css">
+	  <meta name='viewport' content='width=device-width, initial-scale=1'>
+	</head>
+	<body>
+	  <div class="container">
+	    <div class="branding">
+	      <div class="brand-group">
+	        <p id="logo"></p>
+	        <h1>orien<span>tanda</span></h1>
+	      </div>
+	      <button onclick="window.location.href=<?php echo $loginUrl; ?>" name="Login" id="login-btn">Login</button>
+	    </div>
+	  </div>
+	</body>
