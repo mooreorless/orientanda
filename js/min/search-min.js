0ddf7cf3d9e1978f045a8f305aaec4b29ba350e0
Leon Pearce <leonp1991@gmail.com>
2016-04-23 13:44:58 +1000

    updated login, search ctrl

---
+++ js/min/search-min.js
@@ -0,0 +1,27 @@
+let [users, roles] = [[],[]]
+
+function importData(jsonUsers, jsonRoles) {
+  users = JSON.parse(jsonUsers);
+
+  let nameList = document.getElementById('nameList')
+  let surnameList = document.getElementById('surnameList')
+
+  console.log(users);
+
+  for (let i = 0, l = users.length; i < l; i++) {
+    let user = users[i]
+
+
+
+    // names = user.name.split(" ")
+    //
+    // option = document.createElement('option')
+    // option.value = name[0]
+    // option.innerHTML = name[0]
+    // nameList.appendChild(option)
+  }
+
+  }
+}
+
+
--- pages/search.php
+++ pages/search.php
@@ -3,33 +3,35 @@
 	<style type="text/css"> body {font-family:Arial, Sans-Serif;} #container {width:300px; margin:250 auto;} form label {display:inline-block; width:140px;}
    form input[type="text"], form input[type="password"], form input[type="email"] {width:160px;} form .line {clear:both;}
    form .line.submit {text-align:right;} </style>
+
+   <script src="../js/search.js" type="text/javascript"></script>
 </head>
 
 <body>
-<div id ="container">
-	<form>
-		Name: <select id="namelist" name = "namelist" form="nameform">
-				<option value ="-">-</option>
-				<option value ="bob">Bob</option>
-				<option value ="mark">Mark</option>
-				<option value ="123456789">123456789101112</option>
-			</select><br>
-		Surname: <select id="surnamelist" name = "surnamelist" form="surnameform">
-				<option value ="-          ">-</option>
-				<option value ="Johnson">Johnson</option>
-				<option value ="Smith">Smith</option>
-				<option value ="123456789">123456789101112</option>
-				</select><br>
-		Role: <select id="rolelist" name = "rolelist" form="roleform">
-				<option value ="-">-</option>
-				<option value ="sales manager">Sales Manager</option>
-				<option value ="boss">Boss</option>
-				<option value ="123456789">123456789101112</option>
-				</select><br>
 
-	</form>
-</div>
+  <div id ="container">
+  	<form>
+  		Name: <select id="nameList" name = "namelist" form="nameform">
+  				<option value ="-">-</option>
+  				<option value ="bob">Bob</option>
+  				<option value ="mark">Mark</option>
+  				<option value ="123456789">123456789101112</option>
+  			</select><br>
+  		Surname: <select id="surnameList" name = "surnamelist" form="surnameform">
+  				<option value ="-          ">-</option>
+  				<option value ="Johnson">Johnson</option>
+  				<option value ="Smith">Smith</option>
+  				<option value ="123456789">123456789101112</option>
+  				</select><br>
+  		Role: <select id="rolelist" name = "rolelist" form="roleform">
+  				<option value ="-">-</option>
+  				<option value ="sales manager">Sales Manager</option>
+  				<option value ="boss">Boss</option>
+  				<option value ="123456789">123456789101112</option>
+  				</select><br>
 
+  	</form>
+  </div>
 
-
+  <script type="text/javascript"> importData('<?php echo $data; ?>') </script>
 </body>
