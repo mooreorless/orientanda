e320b18e5473fd217a973d5dc7e166a245a15082
Leon Pearce <leonp1991@gmail.com>
2016-04-23 12:41:38 +1000

    added srch ctrl

--- controllers/login.ctrl.php
+++ controllers/login.ctrl.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
 
-  header('location: )
+  header('location:../pages/search.php');
 
   $appId = '1de243ffe194e0838c4ffe8a585b4a503eff2c4c0f403930ed5c3cd40e71d010';
   $appSecret = 'eb4f202ffcf30f1272dae89642337d7d3fd94f3f1cde5b27fabc3acb2d3a94a1';
