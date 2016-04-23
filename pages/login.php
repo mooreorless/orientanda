3f15736db6ef6c0851dec0c4a39c3ced7cef17a9
Elliott Moore <elliott.moore@moretonbay.qld.gov.au>
2016-04-23 10:14:53 +1000

    added base login markup

---
+++ pages/login.php
@@ -0,0 +1,14 @@
+<div class="overlay">
+  <div class="modal">
+    <p class="btn-close">
+      <i class="material-icons">clear</i>
+    </p>
+    <h2 class="article-head">Login</h2>
+    <hr class="article-title-rule">
+    <form action="submit" class="login-form">
+      <input type="text" name="name" placeholder="Name" id="name">
+      <input type="password" name="password" placeholder="Password" id="password">
+      <input type="submit" name="login" value="Login" id="login">
+    </form>
+  </div>
+</div> <!-- login overlay -->
